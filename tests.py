82d04c3dde649892c7de767e28b1cfd19f7823d7
ValeriyMakarov <misteranon.com@gmail.com>
2023-12-30 20:28:14 +0200

    comments

--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
-import mcmanager
 import unittest
+
+import mcmanager
+from mcmanager import InvalidCountOfArgumentsError
+from mcmanager import InvalidCommandError
 
 STYLE_RESET, STYLE_SUCCESS = '\033[0m', '\033[32m'
 
@@ -11,8 +14,16 @@ def info(msg: str):
 class MinecraftManager(unittest.TestCase):
     def tearDown(self) -> None:
         print(STYLE_RESET)
-
+    # проверить все ошибки (негативные)
+    # проверить все рабочие комбинации
     # Модульные тесты
+    def testa(self):
+        with self.assertRaises(InvalidCommandError) as e:
+            mcmanager.parse_args('')
+        print(e.exception)
+        self.assertEqual(str(e.exception), "Неверная команда. Список команд: ['get', 'help', 'ls', 'move'].")
+        ...
+
     def test_args_parser_from_str(self):
         command, keys, name, path = mcmanager.parse_args('mcmanager ap move gregtech')
 
